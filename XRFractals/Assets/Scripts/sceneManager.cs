6fdc54a2436198879bbfd7aedbf5a4ad840d0508
eozinche <ec1958@nyu.edu>
2019-01-20 02:26:18 -0500

    Multiple portal opening test

---
+++ XRFractals/Assets/Scripts/sceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneManager : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		//tap to create portal
+
+
+	}
+}
