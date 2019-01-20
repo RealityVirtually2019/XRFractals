b0c5bf19a5debaad16feefc511b982fc74e74394
Sukhraj <37422375+sukhraj-johal@users.noreply.github.com>
2019-01-20 00:46:44 -0500

    Added the plus and reset buttons as well as the graphics, basic functionality is implemented.

---
+++ XRFractals/Assets/Scr_PlusButtonClicked.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_PlusButtonClicked : MonoBehaviour {
+
+    public void OnClick()
+    {
+        //base functionality for when the plus button is clicked
+
+    }
+}
---
+++ XRFractals/Assets/Scr_ResetButtonClicked.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_ResetButtonClicked : MonoBehaviour {
+
+    public void OnClick()
+    {
+        //put functionality for when the reset button is clicked 
+
+        //i dont know if reset the scene could work, using 
+        //SceneManager.LoadScene(0); - the first one in build index
+    }
+}
