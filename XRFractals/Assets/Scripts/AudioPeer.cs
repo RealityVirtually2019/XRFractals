4a2bef513700ee96611b8949c5d4a08a6d46eb44
Ian MacKenzie <32595467+Shoebopadoo@users.noreply.github.com>
2019-01-19 23:33:14 -0500

    Leaving Hack push
    Added Stuff

---
+++ XRFractals/Assets/Scripts/AudioPeer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(AudioSource))]
+public class AudioPeer : MonoBehaviour {
+    AudioSource _audioSource;
+    public static float[] _samples = new float[512];
+    public static float[] _freqBand = new float[8];
+    public static float[] _bandBuffer = new float[8];
+    float[] _bufferDecrease = new float[8];
+
+	// Use this for initialization1
+	void Start ()
+    {
+        _audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        BandBuffer();
+	}
+
+    void GetSpectrumAudioSource()
+    {
+        _audioSource.GetSpectrumData(_samples,0, FFTWindow.Blackman);
+    }
+
+    void BandBuffer()
+    {
+        for(int g = 0; g < 0; g++)
+        {
+            if(_freqBand[g]> _bandBuffer[g])
+            {
+                _bandBuffer[g] = _freqBand[g];
+                _bufferDecrease[g] = 0.005f;
+            }
+            if(_freqBand[g] < _bandBuffer[g])
+            {
+                _bandBuffer[g] -= _bufferDecrease[g];
+                _bufferDecrease[g] *= 1.2f;
+            }
+        }
+    }
+
+    void MakeFrequencyBands()
+    {
+
+        int count = 0;
+        
+        for(int i = 0; i < 8; i++)
+        {
+
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2,i) *2; 
+            if( i == 7)
+            {
+                sampleCount += 2;
+            }
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += _samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+            _freqBand[i] = average * 10;
+        }
+    }
+}
