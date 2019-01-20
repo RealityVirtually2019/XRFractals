37e481ea77604e9c7c3acd4a5aff74c655427b57
eozinche <ec1958@nyu.edu>
2019-01-19 21:16:36 -0500

    Merged AR kit and test scene

---
+++ XRFractals/Assets/Scripts/AR/AudioController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+    public AudioSource ambientSound;
+
+    // Attach this to the camera 
+
+    void Start()
+    {
+        ambientSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Hit!");
+        if (other.gameObject.tag == "Door")
+        {
+            Debug.Log("Hitted the door!");
+            if (!ambientSound.isPlaying) ambientSound.Play();
+            else ambientSound.Stop();
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        //ambientSound.Stop();
+    }
+
+}
+
---
+++ XRFractals/Assets/Scripts/AR/Swim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swim : MonoBehaviour {
+
+	private Animator animation;
+
+	// Use this for initialization
+	void Start () {
+		animation = GetComponent<Animator>();
+		animation.Play ("cruscarp_skel|swim",0,0.0f);
+		// animation.Play ("movement",0,0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
