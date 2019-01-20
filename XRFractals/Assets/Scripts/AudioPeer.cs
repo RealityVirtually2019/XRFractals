2f07cd86118c6f7e1c5b8c39026410cdb9e5bf6b
Ian MacKenzie <32595467+Shoebopadoo@users.noreply.github.com>
2019-01-20 01:29:04 -0500

    Updated AudioPeer
    New logic, let make these fractals dance

--- XRFractals/Assets/Scripts/AudioPeer.cs
+++ XRFractals/Assets/Scripts/AudioPeer.cs
@@ -2,18 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof(AudioSource))]
+[RequireComponent(typeof(AudioSource))]
 public class AudioPeer : MonoBehaviour {
     AudioSource _audioSource;
-    public static float[] _samples = new float[512];
-    public static float[] _freqBand = new float[8];
-    public static float[] _bandBuffer = new float[8];
+    float[] _samples = new float[512];
+    float[] _freqBand = new float[8];
+    float[] _bandBuffer = new float[8];
     float[] _bufferDecrease = new float[8];
 
-	// Use this for initialization1
-	void Start ()
+    private float[] _freqBandHighest = new float[8];
+    public static float[] _audioBand = new float[8];
+    public static float[] _audioBandBuffer = new float[8];
+
+    public static float[] _amplitude = new float[8];
+    public static float[] _amplitudeBuffer = new float[8];
+
+    public static float _Amplitude, _AmplitudeBuffer;
+    float _AmplitudeHighest;
+    public float _audioProfile;
+
+
+    // Use this for initialization1
+    void Start ()
     {
         _audioSource = GetComponent<AudioSource>();
+        AudioProfile(_audioProfile);
+
 	}
 	
 	// Update is called once per frame
@@ -22,7 +36,47 @@ public class AudioPeer : MonoBehaviour {
         GetSpectrumAudioSource();
         MakeFrequencyBands();
         BandBuffer();
+        CreateAudioBand();
+        GetAmplitude();
 	}
+
+    void AudioProfile(float _audioProfile)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            _freqBandHighest[i] = _audioProfile;
+        }
+    }
+
+    void GetAmplitude()
+    {
+        float _currentAmplitude = 0;
+        float _currentAmplitudeBuffer = 0;
+        for(int i = 0; i< 8; i++)
+        {
+            _currentAmplitude += _audioBand[i];
+            _currentAmplitudeBuffer += _audioBandBuffer[i];
+        }
+        if(_currentAmplitude > _AmplitudeHighest)
+        {
+            _AmplitudeHighest = _currentAmplitude;
+        }
+        _Amplitude = _currentAmplitude / _AmplitudeHighest;
+        _AmplitudeBuffer = _currentAmplitudeBuffer / _AmplitudeHighest;
+    }
+
+    void CreateAudioBand()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if(_freqBand[i] > _freqBandHighest[i])
+            {
+                _freqBandHighest = _freqBand;
+            }
+            _audioBand[i] = (_freqBand[i] / _freqBandHighest[i]);
+            _audioBandBuffer[i] = (_freqBand[i] / _freqBandHighest[i]);
+        }
+    }
 
     void GetSpectrumAudioSource()
     {
